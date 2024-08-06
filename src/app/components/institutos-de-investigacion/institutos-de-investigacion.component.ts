12b5884f17c4fb2fb68e3b3c7c06a9372af146fd
Ernesto2021Me <carlosmarcial9ahuajintepec@gmail.com>
2024-08-05 21:20:10 -0600

    actualizacion
    actualizacion

--- src/app/components/institutos-de-investigacion/institutos-de-investigacion.component.ts
+++ src/app/components/institutos-de-investigacion/institutos-de-investigacion.component.ts
@@ -28,24 +28,5 @@ export class InstitutosDeInvestigacionComponent implements OnInit {
     return this.openIndex === index;
   }
 
-  
-  campusPanel = {
-    title: 'Campus Huajuapan de León',
-    isOpen: false,
-    institutos: [
-      { nombre: 'Instituto de Computación', url: 'home/investigacion/institutos_de_investigacion/instituto_de_computacion' },
-      { nombre: 'Instituto de Electrónica y Mecatrónica', url: 'home/investigacion/institutos_de_investigacion/instituto_de_electronica_y_mecatronica' },
-      { nombre: 'Instituto de Diseño', url: 'home/investigacion/institutos_de_investigacion/instituto_de_diseno' },
-      { nombre: 'Instituto de Física y Matemáticas', url: 'home/investigacion/institutos_de_investigacion/instituto_de_fisica_y_matematicas' },
-      { nombre: 'Instituto de Ciencias Sociales y Humanidades', url: 'home/investigacion/institutos_de_investigacion/instituto_de_ciencias_sociales_y_humanidades' },
-      { nombre: 'Instituto de Agroindustrias', url: 'home/investigacion/institutos_de_investigacion/instituto_de_agroindustrias' },
-      { nombre: 'Instituto de Hidrología', url: 'home/investigacion/institutos_de_investigacion/instituto_de_hidrologia' },
-      { nombre: 'Instituto de Minería', url: 'home/investigacion/institutos_de_investigacion/instituto_de_mineria' },
-      { nombre: 'Instituto de Ingeniería Industrial y Automotriz', url: 'home/investigacion/institutos_de_investigacion/instituto_de_ingenieria_industrial_y_automotriz' },
-      { nombre: 'Centro de Estudios de Nuevos Materiales', url: 'home/investigacion/institutos_de_investigacion/centro_de_estudios_de_muevos_materiales' },
-    ]
-  };
-  
-
 
 }
