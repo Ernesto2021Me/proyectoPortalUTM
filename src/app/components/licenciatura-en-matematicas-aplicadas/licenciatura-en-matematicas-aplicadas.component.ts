71ed4f61ac383d49fb48963076f9442a87f3460e
Ernesto2021Me <carlosmarcial9ahuajintepec@gmail.com>
2024-08-07 16:09:32 -0600

    actualizacion
    actualizacion

--- src/app/components/licenciatura-en-matematicas-aplicadas/licenciatura-en-matematicas-aplicadas.component.ts
+++ src/app/components/licenciatura-en-matematicas-aplicadas/licenciatura-en-matematicas-aplicadas.component.ts
@@ -33,133 +33,25 @@ export class LicenciaturaEnMatematicasAplicadasComponent implements OnInit {
     return this.openIndex === index;
   }
 accordionPanels = [
-    {
-      title: 'JEFATURA DE CARRERA',
-      content: `
-        <div class="jefatura-carrera">
-          <p>Dr. Franco Barragán Mendoza</p>
-          <p><a href="mailto:jcmatematicasaplicadas@mixteco.utm.mx">jcmatematicasaplicadas@mixteco.utm.mx</a></p>
-        </div>
-      `,
-      isOpen: false
-    },
-    { 
-      title: 'MISIÓN', 
-      content: `
-        ...
-      `, 
-      isOpen: false 
-    },
-    { 
-      title: 'VISIÓN', 
-      content: `
-        ...
-      `, 
-      isOpen: false 
-    },
-    { 
-      title: 'OBJETIVO', 
-      content: `
-        ...
-      `, 
-      isOpen: false 
-    },
+     
     {
       title: 'PERFIL DE INGRESO',
       content: `
-        <div class="perfil-ingreso-content">
-          <p><strong>El aspirante a cursar la Licenciatura en Matemáticas Aplicadas en la UTM deberá tener los siguientes conocimientos, habilidades y actitudes y valores:</strong></p>
-          <p><strong>Conocimientos:</strong></p>
-          <ul>
-            <li>Aritmética.</li>
-            <li>Álgebra básica.</li>
-            <li>Trigonometría.</li>
-            <li>Geometría.</li>
-          </ul>
-          <p><strong>Habilidades para:</strong></p>
-          <ul>
-            <li>Realizar razonamientos y emitir juicios.</li>
-            <li>Resolver e interpretar problemas.</li>
-            <li>Trabajar en equipo y de forma individual.</li>
-          </ul>
-          <p><strong>Actitudes y valores:</strong></p>
-          <ul>
-            <li>Interés por el estudio de las matemáticas.</li>
-            <li>Gusto por enfrentar retos donde sea necesario el uso de razonamientos lógicos.</li>
-            <li>Actitud positiva, apertura para conocer nuevos conceptos, procesos y técnicas.</li>
-            <li>Ser perseverante y persistente para adquirir conocimiento.</li>
-            <li>Comprensión, respeto, aprecio y tolerancia hacia la diversidad.</li>
-          </ul>
-        </div>
+        
       `,
       isOpen: false
     },    
     {
       "title": "PERFIL DE EGRESO",
       "content": `
-        <div class="perfil-egreso-content">
-          <p><strong>El Licenciado en Matemáticas Aplicadas, es un profesional con una sólida formación en Matemáticas y sus aplicaciones actuales, posee gran capacidad de abstracción de la realidad, es capaz de interpretar, crear y resolver modelos matemáticos. Tiene una preparación de calidad que le permite realizar estudios de posgrado en el país y el extranjero. Al mismo tiempo, se desempeña en el sector público y de servicios, en centros de investigación científica y aplicada.</strong></p>
-          
-          <p><strong>Conocimientos:</strong></p>
-          <ul>
-            <li>Sólidos en las áreas del conocimiento: Análisis Matemático, Álgebra, Geometría y Topología, Probabilidad y Estadística y Modelado Matemático.</li>
-            <li>Básicos de Programación, Inglés, Mecánica Clásica, Economía Matemática y Lógica Matemática.</li>
-            <li>Sólidos en por lo menos una de las líneas terminales con orientación a: Ciencia de Datos e Inteligencia Artificial, Economía y Finanzas, Aproximación y Optimización, Topología y Sistemas Dinámicos, Biología Matemática, Probabilidad y Estadística.</li>
-            <li>Del enfoque axiomático y de los métodos de validación en la construcción de las teorías matemáticas, y que sustentarán cualquier tipo de investigación aplicada.</li>
-            <li>De los conceptos, métodos y teorías de las áreas fundamentales de las matemáticas, además de la diversa gama de posibilidades de aplicación.</li>
-          </ul>
-          
-          <p><strong>Habilidades:</strong></p>
-          <ul>
-            <li>Construir y desarrollar argumentaciones lógicas con una identificación clara de hipótesis y conclusiones, así como de identificación de inconsistencias.</li>
-            <li>Discernir el desarrollo lógico de teorías matemáticas y abstraer las relaciones entre ellas.</li>
-            <li>Aplicar la matemática y sus estructuras lógicas, para resolver problemas de las ciencias exactas, naturales, sociales y/o la tecnología.</li>
-            <li>Construir modelos matemáticos simplificados que describan una situación compleja, identificando sus elementos esenciales y efectuando las aproximaciones necesarias; conjeturando y creando estrategias de solución, analíticas y/o numéricas.</li>
-            <li>Extraer información a partir de grandes volúmenes de datos, analizar y realizar predicciones.</li>
-            <li>Generar procesos o proyectos de difusión u divulgación del conocimiento y valor de la matemática que impacten en su comunidad, así como elaborar e impartir cursos de matemáticas a distintos niveles.</li>
-            <li>Razonar de manera estructurada, comunicar con claridad y precisión conceptos, procesos de investigación y resultados científicos en lenguaje oral y escrito ante sus pares, y en situaciones de enseñanza y de divulgación.</li>
-          </ul>
-          
-          <p><strong>Actitudes y valores:</strong></p>
-          <ul>
-            <li>Desarrollar los valores éticos de la profesión que le permitan actuar adecuadamente dentro del campo laboral y social de manera cooperativa y colaborativa.</li>
-            <li>Abordar los conflictos a través del diálogo y la negociación, ejerciendo los valores del pluralismo, democracia, equidad, solidaridad, tolerancia y paz.</li>
-            <li>Ser líder íntegro y humanista, promotor de la convivencia multicultural y capaz de tener apertura al cambio, comprensión y tolerancia hacia la diversidad.</li>
-            <li>Desarrollar investigación con responsabilidad social en equipos interdisciplinarios.</li>
-            <li>Desarrollar un pensamiento abierto y flexible, con capacidad de asombro, que le permita la integración de nuevos saberes, para un aprendizaje a lo largo de la vida.</li>
-          </ul>
-        </div>
+        
       `,
       "isOpen": false
     },    
     {
       title: 'CAMPO DE ACCIÓN',
       content: `
-        <div class="campo-de-accion-content">
-          <p>El Licenciado en Matemáticas Aplicadas tendrá conocimientos sólidos y habilidades para integrarse, entre otros, en los ámbitos laborales siguientes:</p>
-          <ul>
-            <li>Como profesionista independiente:</li>
-            <ul>
-              <li>Consultor en estadística, control de calidad y estudios de factibilidad.</li>
-              <li>Dirección de empresas de investigación de mercados y estudios de opinión.</li>
-              <li>Asesor para el desarrollo y la administración económica de empresas.</li>
-              <li>Diseñador de software.</li>
-            </ul>
-            <li>Como miembro de una Empresa o Industria:</li>
-            <ul>
-              <li>Empresas públicas o privadas que requieren de análisis numéricos y métodos computacionales, análisis de datos e inteligencia artificial para análisis de mercado y toma de decisiones.</li>
-              <li>Control y optimización de procesos productivos en diversas empresas o industrias.</li>
-              <li>Simulación de sistemas, investigación de operaciones y experimentación en la industria.</li>
-              <li>Empresas dedicadas al desarrollo de software o departamentos de planeación y logística.</li>
-            </ul>
-            <li>Como miembro de alguna Institución u Organización:</li>
-            <ul>
-              <li>Catedrático y/o investigador en instituciones educativas públicas o privadas a nivel medio superior y superior.</li>
-              <li>En organismos que se dediquen a recolectar, organizar, analizar e interpretar grandes cantidades de datos.</li>
-              <li>Tendrá los conocimientos suficientes para integrarse a programas afines de posgrados de excelencia, dentro y fuera del país.</li>
-            </ul>
-          </ul>
-        </div>
+        
       `,
       isOpen: false
     },    
