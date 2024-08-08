cc1fe6afbb74c67fbc49a24e92adefe8430beb8e
Ernesto2021Me <carlosmarcial9ahuajintepec@gmail.com>
2024-08-08 10:03:38 -0600

    accion
    accion

--- src/app/components/ingenieria-quimica/ingenieria-quimica.component.ts
+++ src/app/components/ingenieria-quimica/ingenieria-quimica.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingenieria-quimica.component.css']
 })
 export class IngenieriaQuimicaComponent implements OnInit {
-
+  panels: { title: string, content: string }[] = [];
+  openIndex: number | null = null;
   constructor() { }
 
   ngOnInit(): void {
+    this.panels = [
+      { title: 'JEFATURA DE CARRERA', content: 'Contenido del panel 1' },
+      { title: 'MISIÓN', content: 'Contenido del panel 2' },
+      { title: 'VISIÓN', content: 'Contenido del panel 3' },
+      { title: 'OBJETIVO', content: 'Contenido del panel 4' },
+      { title: 'PERFIL DE INGRESO', content: 'Contenido del panel 5' },
+      { title: 'PERFIL DE EGRESO', content: 'Contenido del panel 6' },
+      { title: 'CAMPO DE ACCIÓN', content: 'Contenido del panel 7' },
+      { title: 'PLAN DE ESTUDIOS', content: 'Contenido del panel 8' },
+    
+  ];
+  
+  }
+  toggle(index: number): void {
+    this.openIndex = this.openIndex === index ? null : index;
+  }
+  
+  isOpen(index: number): boolean {
+    return this.openIndex === index;
   }
 
 }
